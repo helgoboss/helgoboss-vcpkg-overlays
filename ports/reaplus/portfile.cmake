c306a6d881988599c292b2712bd16be409674982
Benjamin Klum <benjamin.klum@helgoboss.org>
2020-01-07 19:52:34 +0100

    Update SHA512

--- ports/reaplus/portfile.cmake
+++ ports/reaplus/portfile.cmake
@@ -3,7 +3,7 @@ vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO helgoboss/reaplus
     REF 8cf6e22319674824401d5d25bb3d5de8f28f5cff
-    SHA512 1
+    SHA512 0ec83b2ecd8fc0fb961c71daacc73b2aa770df1adbbaea552ff35bf1183b2cda7c94757fca84e72f11296d34a893e93d1dcfe186c5fa85fa8b7a2e562bbbd6e3
     HEAD_REF master
 )
 vcpkg_configure_cmake(
